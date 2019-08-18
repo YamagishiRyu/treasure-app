56a710364308446196b8d00d0feb22e11c337aca
YamagishiRyu <bubusu.ryu@gmail.com>
2019-08-18 09:43:48 +0900

    :zap: Add: read file method

--- middle-app/models/mdfile.go
+++ middle-app/models/mdfile.go
@@ -1,9 +1,24 @@
 package models
 
+import (
+	"io/ioutil"
+
+	"github.com/labstack/gommon/log"
+)
+
 type Mdfile struct {
 	ID           int64  `db:"id" json:"id"`
 	Name         string `db:"name" json:"name"`
 	Path         string `db:"path" json:"path"`
 	URL          string `db:"url" json:"url"`
 	RepositoryID int64  `db:"repository_id" json:"repository_id"`
+}
+
+func (m Mdfile) GetText() (string, error) {
+	data, err := ioutil.ReadFile(m.Path)
+	if err != nil {
+		log.Error(err)
+		return "", err
+	}
+	return string(data), nil
 }
