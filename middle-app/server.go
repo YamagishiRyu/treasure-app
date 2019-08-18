2afb6629b2b32330917dbabfd3221c30862ead07
YamagishiRyu <bubusu.ryu@gmail.com>
2019-08-19 08:45:12 +0900

    :zap: Update for cors

--- middle-app/server.go
+++ middle-app/server.go
@@ -9,9 +9,12 @@ import (
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/jmoiron/sqlx"
+	"github.com/justinas/alice"
+	"github.com/rs/cors"
 
 	controllers "github.com/YamagishiRyu/treasure-app/middle-app/controllers"
 	db "github.com/YamagishiRyu/treasure-app/middle-app/db"
+	"github.com/YamagishiRyu/treasure-app/middle-app/middleware"
 )
 
 type Server struct {
@@ -46,14 +49,23 @@ func (s *Server) Run(addr string) {
 
 func (s *Server) Route() *mux.Router {
 	r := mux.NewRouter()
+	corsMiddleware := cors.New(cors.Options{
+		AllowedOrigins: []string{"*"},
+		AllowedHeaders: []string{"Authorization"},
+	})
+
+	commonChain := alice.New(
+		middleware.RecoverMiddleware,
+		corsMiddleware.Handler,
+	)
 
 	repositoryController := controllers.NewRepositoryController(s.dbx)
 	repo := r.PathPrefix("/repositories").Subrouter()
 
-	repo.Methods(http.MethodGet).Path("/").Handler(AppHandler{repositoryController.Index})
-	repo.Methods(http.MethodGet).Path("/{id}").Handler(AppHandler{repositoryController.Show})
-	repo.Methods(http.MethodGet).Path("/clone").Handler(AppHandler{repositoryController.Create})
-	repo.Methods(http.MethodGet).Path("/search").Handler(AppHandler{repositoryController.Search})
+	repo.Methods(http.MethodGet).Path("/").Handler(commonChain.Then(AppHandler{repositoryController.Index}))
+	repo.Methods(http.MethodGet).Path("/{id}").Handler(commonChain.Then(AppHandler{repositoryController.Show}))
+	repo.Methods(http.MethodGet).Path("/clone").Handler(commonChain.Then(AppHandler{repositoryController.Create}))
+	repo.Methods(http.MethodGet).Path("/search").Handler(commonChain.Then(AppHandler{repositoryController.Search}))
 
 	return r
 }
